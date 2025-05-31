bf40f4a1c4514f31f13d63415d19104875bb1807
Gokhulnath <58549927+Gokhulnath@users.noreply.github.com>
2025-05-31 01:51:21 -0500

    create new chat for new user

--- manus-frontend/app/page.tsx
+++ manus-frontend/app/page.tsx
@@ -3,6 +3,10 @@ import { useEffect, useState } from "react";
 import ChatHistory from "./components/ChatHistory";
 import ChatPage from "./components/ChatPage";
 import { postData } from "@/lib/api";
+import { paths } from "@/lib/openapi-types";
+
+type ChatCreate =
+  paths["/chats/"]["post"]["requestBody"]["content"]["application/json"];
 
 export default function Home() {
   const [selectedChatId, setSelectedChatId] = useState<string>("");
@@ -14,15 +18,17 @@ export default function Home() {
         setSelectedChatId(storedId);
       } else {
         try {
-          const newChat = await postData("/chats/", { title: "Untitled Chat" });
-          setSelectedChatId(newChat.id);
-          localStorage.setItem("selectedChatId", newChat.id);
+          const newChat: ChatCreate = {
+            title: "Untitled Chat",
+          };
+          const response = await postData("/chats/", newChat);
+          setSelectedChatId(response.id);
+          localStorage.setItem("selectedChatId", response.id);
         } catch (err) {
           console.error("Failed to create initial chat:", err);
         }
       }
     };
-
     initChat();
   }, []);
 
@@ -39,7 +45,8 @@ export default function Home() {
         localStorage.setItem("selectedChatId", id);
       }} chat_id={selectedChatId} />
       <ChatPage chat_id={selectedChatId} />
-      <div className="min-w-[33vw] hidden lg:block h-full bg-white rounded-2xl p-5 border border-gray-200">
+      <div className="min-w-[33vw] hidden  h-full bg-white rounded-2xl p-5 border border-gray-200">
+        {/* lg:block */}
         Manus Doc Viewer
       </div>
     </div>
